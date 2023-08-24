1fd6fa4a56865185de9624a0b4d90b3d57cfca7a
[Flutter_SakuSaku13:44] <[gitonly543@gmail.com]>
2023-08-24 11:35:59 +0900

    firsto commit

---
+++ app/src/main/java/com/example/jetprofile/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.example.jetprofile
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color// Textのカラーはこれをimportする
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import com.example.jetprofile.ui.theme.JetProfileTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            JetProfileTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Text(
+                        text = "Jboy Hashimoto",
+                        color = Color.Gray,
+                        fontSize = 20.sp,
+                        fontWeight = FontWeight.ExtraBold,
+                        )
+                }
+            }
+        }
+    }
+}
