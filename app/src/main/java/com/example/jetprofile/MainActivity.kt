511f2c977070f2036071ce440f01049a56d2cf2a
[Flutter_SakuSaku13:44] <[gitonly543@gmail.com]>
2023-08-24 13:44:32 +0900

    refactor: @Composableでコンポーネント化

--- app/src/main/java/com/example/jetprofile/MainActivity.kt
+++ app/src/main/java/com/example/jetprofile/MainActivity.kt
@@ -4,15 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.LocationOn
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color// Textのカラーはこれをimportする
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -77,14 +82,8 @@ class MainActivity : ComponentActivity() {
                             )
                             Spacer(modifier = Modifier.height(20.dp))
 
-                            Row(verticalAlignment = Alignment.CenterVertically) {
-                                Icon(
-                                    imageVector = Icons.Default.Email,
-                                    contentDescription = "email"
-                                )
-                                Spacer(modifier = Modifier.width(10.dp))
-                                Text(text = "Email", fontSize = 14.sp, fontWeight = FontWeight.Bold)
-                            }
+                            // Email
+                            Label(icon = Icons.Default.Email, text = "Email")
                             Spacer(modifier = Modifier.height(10.dp))
                             Text(text = "example.com", fontSize = 16.sp)
                             Spacer(modifier = Modifier.height(5.dp))
@@ -106,9 +105,44 @@ class MainActivity : ComponentActivity() {
                             onClick = {}) {
                             Text(text = "詳細を表示", color = Color.White)
                         }
+                        Spacer(modifier = Modifier.height(20.dp))
+
+                        // 趣味 & 居住地セクション
+                        Column (
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .clip(RoundedCornerShape(10.dp))
+                                .background(Color.LightGray.copy(alpha = 0.3f))
+                                .padding(horizontal = 10.dp, vertical = 20.dp)
+                        )
+                        {
+                            Label(
+                                icon = Icons.Default.Favorite,
+                                text = "趣味: 旅行, Zennの記事を書く",
+                            )
+                            Spacer(modifier = Modifier.height(10.dp))
+                            Label(
+                                icon = Icons.Default.LocationOn,
+                                text = "居住地: 東京都",
+                                color = Color.Gray,
+                            )
+                        }
                     }
                 }
             }
         }
+    }
+}
+
+// アノテーションをつけるとJetpack Composeで使える
+@Composable
+fun Label(icon: ImageVector, text: String, color: Color = MaterialTheme.colors.onBackground) {
+    Row(verticalAlignment = Alignment.CenterVertically) {
+        Icon(
+            imageVector = icon,
+            contentDescription = null,
+        )
+        Spacer(modifier = Modifier.width(10.dp))
+        Text(text = text, color = color, fontSize = 14.sp, fontWeight = FontWeight.Bold)
     }
 }
