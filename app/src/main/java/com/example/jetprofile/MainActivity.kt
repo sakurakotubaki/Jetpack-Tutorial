322bebe11b78e32a86bf99c873982352f76b5943
[Flutter_SakuSaku13:44] <[gitonly543@gmail.com]>
2023-08-24 13:13:34 +0900

    style: Emailを追加

--- app/src/main/java/com/example/jetprofile/MainActivity.kt
+++ app/src/main/java/com/example/jetprofile/MainActivity.kt
@@ -6,9 +6,9 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Email
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -73,7 +73,27 @@ class MainActivity : ComponentActivity() {
                             // 部署・グループ名
                             Text(text = "Android開発部",
                                 color = Color.Gray,
-                                fontSize = 16.sp)
+                                fontSize = 16.sp
+                            )
+                            Spacer(modifier = Modifier.height(20.dp))
+
+                            Row(verticalAlignment = Alignment.CenterVertically) {
+                                Icon(
+                                    imageVector = Icons.Default.Email,
+                                    contentDescription = "email"
+                                )
+                                Spacer(modifier = Modifier.width(10.dp))
+                                Text(text = "Email", fontSize = 14.sp, fontWeight = FontWeight.Bold)
+                            }
+                            Spacer(modifier = Modifier.height(10.dp))
+                            Text(text = "example.com", fontSize = 16.sp)
+                            Spacer(modifier = Modifier.height(5.dp))
+
+                            Divider(
+                                color = Color.LightGray,
+                                thickness = 2.dp,
+                                modifier = Modifier.clip(RoundedCornerShape(1000.dp))
+                            )
                         }
                     }
                 }
